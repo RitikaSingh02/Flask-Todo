5d6811066f1bb5afdb5d0cfbd4f7b893ce3f7d63
RitikaSingh02 <ritika2002singh@gmail.com>
2021-05-13 19:43:53 +0530

    added update search and deletion

--- app.py
+++ app.py
@@ -47,5 +47,30 @@ def delete(sno):
     return redirect("/")
 
 
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update(sno):
+    if request.method == "POST":
+        title = request.form["title"]
+        desc = request.form["desc"]
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")
+    todo = Todo.query.filter_by(sno=sno).first()
+    return render_template("update.html", todo=todo)
+
+
+@app.route('/search')
+def search():
+    allqueries = Todo.query.all()
+    ans = []
+    for task in allqueries:
+        ans.append([task.sno, task.title, task.desc])
+
+    return {"res": ans}
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
