f2e42e9d41808ca58274124dc33b20d4e66690c9
RitikaSingh02 <ritika2002singh@gmail.com>
2021-05-13 14:19:37 +0530

    adding api endpoint for deletion and rendering

--- app.py
+++ app.py
@@ -1,21 +1,50 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask.wrappers import Request
+from werkzeug.utils import redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask(__name__)
-print(__name__)
+f = open("info.txt", "r")
+# mysql://username:password@host/dbname
+app.config['SQLALCHEMY_DATABASE_URI'] = f.read()
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+print(db)
+
+# define the schema
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno}- {self.desc}-{self.date_created}"
 
 
-@app.route('/products/')
-def products():
-    return 'this is product pages'
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == "POST":
+        title = request.form["title"]
+        desc = request.form["desc"]
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+
+    allqueries = Todo.query.all()
+    return render_template('index.html', allqueries=allqueries)
 
 
-@app.route('/index/')
-def index():
-    return render_template('index.html')
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
 
 
 if __name__ == "__main__":
